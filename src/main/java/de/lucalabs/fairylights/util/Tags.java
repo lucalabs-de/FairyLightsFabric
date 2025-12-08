e543ff6be9ad9496226bcdafced328e23a336e23
Gian Luca Spitzer <gian.luca.spitzer@rwth-aachen.de>
2025-12-08 23:15:30 +0100

    fix tags, also fixes white string hanging lights recipe

--- src/main/java/de/lucalabs/fairylights/util/Tags.java
+++ src/main/java/de/lucalabs/fairylights/util/Tags.java
@@ -10,21 +10,21 @@ import net.minecraft.util.Identifier;
 public final class Tags {
     public static final TagKey<Item> DYES = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "dyes"));
     public static final TagKey<Item> DYES_WHITE = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "dyes/white"));
-    public static final TagKey<Item> DYES_ORANGE = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "orange_dyes"));
-    public static final TagKey<Item> DYES_MAGENTA = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "magenta_dyes"));
-    public static final TagKey<Item> DYES_LIGHT_BLUE = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "light_blue_dyes"));
-    public static final TagKey<Item> DYES_YELLOW = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "yellow_dyes"));
-    public static final TagKey<Item> DYES_LIME = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "lime_dyes"));
-    public static final TagKey<Item> DYES_PINK = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "pink_dyes"));
-    public static final TagKey<Item> DYES_GRAY = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "gray_dyes"));
-    public static final TagKey<Item> DYES_LIGHT_GRAY = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "light_gray_dyes"));
-    public static final TagKey<Item> DYES_CYAN = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "cyan_dyes"));
-    public static final TagKey<Item> DYES_PURPLE = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "purple_dyes"));
-    public static final TagKey<Item> DYES_BLUE = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "blue_dyes"));
-    public static final TagKey<Item> DYES_BROWN = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "brown_dyes"));
-    public static final TagKey<Item> DYES_GREEN = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "green_dyes"));
-    public static final TagKey<Item> DYES_RED = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "red_dyes"));
-    public static final TagKey<Item> DYES_BLACK = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "black_dyes"));
+    public static final TagKey<Item> DYES_ORANGE = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "dyes/orange"));
+    public static final TagKey<Item> DYES_MAGENTA = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "dyes/magenta"));
+    public static final TagKey<Item> DYES_LIGHT_BLUE = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "dyes/light_blue"));
+    public static final TagKey<Item> DYES_YELLOW = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "dyes/yellow"));
+    public static final TagKey<Item> DYES_LIME = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "dyes/lime"));
+    public static final TagKey<Item> DYES_PINK = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "dyes/pink"));
+    public static final TagKey<Item> DYES_GRAY = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "dyes/gray"));
+    public static final TagKey<Item> DYES_LIGHT_GRAY = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "dyes/light_gray"));
+    public static final TagKey<Item> DYES_CYAN = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "dyes/cyan"));
+    public static final TagKey<Item> DYES_PURPLE = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "dyes/purple"));
+    public static final TagKey<Item> DYES_BLUE = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "dyes/blue"));
+    public static final TagKey<Item> DYES_BROWN = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "dyes/brown"));
+    public static final TagKey<Item> DYES_GREEN = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "dyes/green"));
+    public static final TagKey<Item> DYES_RED = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "dyes/red"));
+    public static final TagKey<Item> DYES_BLACK = TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "dyes/black"));
 
     public static final TagKey<Block> FENCES = TagKey.of(RegistryKeys.BLOCK, Identifier.of("c", "fences"));
 
